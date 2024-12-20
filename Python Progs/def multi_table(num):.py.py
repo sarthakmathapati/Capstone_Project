f9fdc111451e955cd411f7ce605af575a2eb092e
sark <sarthakmathapati@Sarthaks-MacBook-Pro-3.local>
2024-12-21 00:43:43 +0530

    Capstone

--- Python Progs/def multi_table(num):.py.py
+++
@@ -1,8 +0,0 @@
-def solve():
-    t = int(input())
-    for _ in range(t):
-        m, a, b, c = map(int, input().split())
-        total = min(a, m) + min(b, m) + min(c, 2 * m - min(a, m) - min(b, m))
-        print(total)
-
-solve()
