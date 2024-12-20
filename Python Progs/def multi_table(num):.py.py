88da59aef4116eb7c593bcb94c2316bbd0a91a5f
sark <sarthakmathapati@Sarthaks-MacBook-Pro-3.local>
2024-12-20 14:56:39 +0530

    first comment

--- Python Progs/def multi_table(num):.py.py
+++ Python Progs/def multi_table(num):.py.py
@@ -1,4 +1,8 @@
+def solve():
+    t = int(input())
+    for _ in range(t):
+        m, a, b, c = map(int, input().split())
+        total = min(a, m) + min(b, m) + min(c, 2 * m - min(a, m) - min(b, m))
+        print(total)
 
-n = int(input())
-squrs = { 1: 1 for i in range(1, n+1) }
-print(squrs)
+solve()
