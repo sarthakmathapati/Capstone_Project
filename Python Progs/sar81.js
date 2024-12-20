6997546c90b83d6d6b5d27150589d1a30a592c71
sarthakmathapati <sarthakmathapati4@gmail.com>
2024-12-21 00:45:06 +0530

    Updating content

--- Python Progs/sar81.js
+++
@@ -1,5 +0,0 @@
-// Example: Add functionality for the search button
-document.querySelector('.search-button').addEventListener('click', function() {
-    const query = document.querySelector('.search-bar').value;
-    alert('Searching for: ' + query);
-});
